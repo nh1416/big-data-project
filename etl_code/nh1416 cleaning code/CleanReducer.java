9a6e7a1b657f15ebc1788ccb55e94564d41c06a0
Nancy <nh1416@nyu.edu>
2021-04-29 23:05:20 -0400

    Add cleaning and profiling code

---
+++ etl_code/nh1416 cleaning code/CleanReducer.java
@@ -0,0 +1,30 @@
+import java.io.IOException;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.MultipleOutputs;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+
+
+public static class CleanReducer
+	extends Reducer<Text,IntWritable,Text,IntWritable> {
+
+		private IntWritable result = new IntWritable();
+		private MultipleOutputs<Text, IntWritable> mos;
+		private int sum=0;
+		public void reduce(Text key, Iterable<IntWritable> values,Context context) throws IOException, InterruptedException {
+
+			for (IntWritable val : values) {
+				sum += val.get();
+			}
+			context.write(key, new IntWritable(sum));
+		}
+    }
---
+++ profiling_code/nh1416 profiling code/CountRecsReducer.java
@@ -0,0 +1,26 @@
+import java.io.IOException;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+public static class CountRecsReducer extends
+   Reducer<Text, IntWritable, Text, IntWritable> {
+     private IntWritable result = new IntWritable();
+     
+     public void reduce(Text key, Iterable<IntWritable> values,
+     Context context) throws IOException, InterruptedException {
+       int sum = 0;
+       for (IntWritable val : values) {
+         sum += val.get();
+        }
+        result.set(sum);
+        context.write(key, result);
+  }
+ }
